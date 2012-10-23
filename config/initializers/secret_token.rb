a0bc02344a303d9017c87e8718f1b9e85ebf3ced
curebit-mike <mike@curebit.com>
2012-10-22 18:45:38 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '54589520494ed3bc0f09c3f8e370a4aac394e5a829ba7ac8e187761a4e8c02a10188258326a7fff8b0e714d97a62edacd48994655dff51888085fb56006b5fcf'
